cf739e7475d1748f7796a19659496b680619df34
Shruti3004 <mail2shruti.ag@gmail.com>
2023-06-29 17:49:22 +0530

    fix: ui
    Signed-off-by: Shruti3004 <mail2shruti.ag@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ export default function Home() {
     <div className="container flex justify-center items-center flex-col min-h-screen">
       <img src="/C4GT.png" width={400} />
       <h1 className="text-center text-[30px] mt-4 font-bold text-[#294294]">
-        For Innaguration check out the mail for the link
+        If you are one among the inagurators, For Inauguration check out the
+        mail for the link
       </h1>
     </div>
   );
