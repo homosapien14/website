7a9e8a118d0ffa7d28dd7e62b732748fb638b90d
homosapien14 <spma259@gmail.com>
2023-07-06 11:04:15 +0530

    moved font-famiy to global.css and fized extra space in app/page.tsx

--- src/app/C4GT23/page.tsx
+++ src/app/C4GT23/page.tsx
@@ -2,11 +2,10 @@
 import React from "react";
 import { ProductList, Header } from "../../components/";
 import { productList } from "../../assets/constants";
-import styles from "./C4GT23.module.css";
 
 const C4GT23 = () => {
   return (
-    <div className={`c4gt23-container  bg-gray-100${styles.container}`}>
+    <div className={`c4gt23-container  bg-gray-100`}>
       <Header />
       <ProductList productList={productList} />
     </div>
