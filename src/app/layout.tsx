f3f8bb1e5ca26689a4aa103a72ba6781d18eb50c
Swapnil Amin <76989674+homosapien14@users.noreply.github.com>
2023-07-15 10:52:49 +0530

    Fix: changed title for each page (#20)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,17 +2,20 @@ import "./globals.css";
 import { Footer, Navbar } from "@/component/components";
 import React, { PropsWithChildren } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Metadata } from "next";
 
-export const metadata = {
-  title: "Inaguration",
+export const metadata: Metadata = {
+  title: 'Code for GovTech | Digital Public Goods',
   description:
     "Code for GovTech (C4GT) has been introduced as India's first program, aimed at creating a community that can build and contribute to global Digital Public Goods (DPGs).",
-};
+}
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
     <html lang="en">
-      <title>Code for GovTech</title>
+      <head>
+        <link rel='icon' href='/favicon.ico' sizes="any"/>
+      </head>
       <body>
         <Navbar />
         {children}
